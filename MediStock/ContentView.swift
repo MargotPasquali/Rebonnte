0bf7c459b1240924ce0774df1fda7890e277249b
MargotPasquali <margotpasquali1@gmail.com>
2025-03-19 16:51:10 +0100

    modified .gitignore to exclude API Key

--- MediStock/ContentView.swift
+++ MediStock/ContentView.swift
@@ -17,8 +17,7 @@ struct ContentView: View {
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView().environmentObject(SessionStore())
-    }
+#Preview {
+    ContentView()
+        .environmentObject(SessionStore())
 }
